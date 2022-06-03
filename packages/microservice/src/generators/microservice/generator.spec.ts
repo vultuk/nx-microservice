99e3031ff099be11aa4ae035b82e2f125009b694
Simon Skinner <vultuk28@gmail.com>
2022-06-03 04:40:36 +0100

    initial commit

---
+++ packages/microservice/src/generators/microservice/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { MicroserviceGeneratorSchema } from './schema';
+
+describe('microservice generator', () => {
+  let appTree: Tree;
+  const options: MicroserviceGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
---
+++ packages/microservice/src/generators/microservice/schema.d.ts
@@ -0,0 +1,5 @@
+export interface MicroserviceGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
